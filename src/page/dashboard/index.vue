090988dbec99d664e65c837be07f7c969b8fcceb
local_author <androidlongfei@163.com>
2018-04-19 14:18:47 +0800

    增加背景图片

--- src/page/dashboard/index.vue
+++ src/page/dashboard/index.vue
@@ -1,24 +1,37 @@
 <template>
-<div class="">
-    this is dashboard
+<div class="center">
+    <img :src="schoolImg" width="100%" height="850" alt="">
+    <div class="text">
+        <span>学生成绩管理系统</span>
+    </div>
 </div>
 </template>
 
 <script>
+import school from '../../images/school.jpeg'
 export default {
     data() {
         return {
-
+            schoolImg: school
         }
     },
-    methods: {
-
-    },
-    mounted() {
-        console.log('The time is ', this.$moment().format('HH:mm'))
-    }
+    methods: {}
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.center {
+    position: relative;
+    .text {
+        position: absolute;
+        top: 20px;
+        left: 50%;
+        margin-left: -200px;
+        span {
+            color: white;
+            font-size: 50px;
+            text-shadow: 5px 5px 5px #FF0000;
+        }
+    }
+}
 </style>
