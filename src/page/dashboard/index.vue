bec24bece467deeb53ad135fba5489f64b866ee5
local_author <androidlongfei@163.com>
2018-05-02 13:50:28 +0800

    fix 首页

--- src/page/dashboard/index.vue
+++ src/page/dashboard/index.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="center">
-    <img :src="schoolImg" width="100%" height="850" alt="">
+    <!-- <img :src="schoolImg" width="100%" height="850" alt=""> -->
     <div class="text">
         <span class="title">学生成绩管理系统</span>
         <div class="info">
             <section class="user" v-if="this.userName">
-                <span>用户名:{{this.userName}}</span>
+                <span>用户：{{this.userName}}</span>
             </section>
             <section class="name" v-if="this.name">
-                <span>姓名:{{this.name}}</span>
+                <span>姓名：{{this.name}}</span>
             </section>
             <section class="role" v-if="this.roleName">
-                <span>角色:{{this.roleName}}</span>
+                <span>角色：{{this.roleName}}</span>
             </section>
         </div>
     </div>
@@ -48,34 +48,42 @@ export default {
     }
 }
 </script>
+<style lang="css">
+    #app .main-container .center {
+        height: 100%;
+    }
+</style>
 
 <style lang="scss" scoped>
 .center {
+    background-color: rgba(68, 121, 215, 1);
+    // height: 850px;
     position: relative;
     .text {
         position: absolute;
-        top: 20px;
+        top: 200px;
         left: 50%;
         margin-left: -200px;
         span.title {
-            color: white;
+            color: black;
             font-size: 50px;
-            text-shadow: 5px 5px 5px #FF5000;
+            text-shadow: 2px 2px 1px #111;
         }
         .info {
-            margin-top: 10px;
+            margin-top: 30px;
             margin-left: 80px;
             display: flex;
             flex-direction: column;
             align-items: flex-start;
             span {
-                color: white;
-                font-size: 20px;
+                color: black;
+                font-size: 22px;
+                text-shadow: 2px 2px 1px #ccc;
             }
             .name,
             .role,
             .user {
-                margin-top: 15px;
+                margin-top: 30px;
             }
         }
     }
